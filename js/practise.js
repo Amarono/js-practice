993afbfd23db3d544479eee2eecb1777ca5f0b46
Amarono <Amarono1992@gmail.com>
2024-04-01 17:41:56 +0200

    Update practise.js

--- js/practise.js
+++ js/practise.js
@@ -1,30 +1,75 @@
-let a = 5;
-console.log('hello');
+// let a = 5;
+// console.log('hello');
 
-let c = 10;
-let b = 5;
-console.log(c === b);
+// let c = 10;
+// let b = 5;
+// console.log(c === b);
 
-const hours = 14;
-const minutes = 20;
-let message;
+// const hours = 14;
+// const minutes = 20;
+// let message;
 
-if (minutes > 0) {
-  message = `${hours} г. ${minutes} хв.`;
-} else {
-  message = `${hours} г.`;
-}
+// if (minutes > 0) {
+//   message = `${hours} г. ${minutes} хв.`;
+// } else {
+//   message = `${hours} г.`;
+// }
 
-// const message = minutes > 0 ? `${hours} г. ${minutes} хв.` : `${hours} г.`;
+// // const message = minutes > 0 ? `${hours} г. ${minutes} хв.` : `${hours} г.`;
 
-console.log(message);
+// console.log(message);
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
 
-Array.toUppercase = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = array[i].toUpperCase();
-  }
+// Array.toUppercase = function (array) {
+//   for (let i = 0; i < array.length; i++) {
+//     array[i] = array[i].toUpperCase();
+//   }
+// };
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+// console.log(aptRating, aptDescr, aptPrice, aptTags);
+// leng;
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+
+// console.log(keys);
+// console.log(values);
+
+const apartment = {
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
 };
+const values = [];
+const keys = Object.keys(apartment);
+for (const key of keys) {
+  values.push(apartment[key]);
+}
+console.log(keys);
+console.log(values);
