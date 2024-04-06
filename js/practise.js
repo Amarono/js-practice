08256be15cbede6e3e377bcdef85958c99b2ade2
Amarono <Amarono1992@gmail.com>
2024-04-06 18:55:21 +0200

    Update practise.js

--- js/practise.js
+++ js/practise.js
@@ -61,15 +61,26 @@
 // console.log(keys);
 // console.log(values);
 
-const apartment = {
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-};
-const values = [];
-const keys = Object.keys(apartment);
-for (const key of keys) {
-  values.push(apartment[key]);
+//  const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+function addOverNum(value, ...args) {
+  let total = 0;
+  for (const arg of args) {
+    if (arg > value) {
+      total += arg;
+    }
+  }
+  return total;
 }
-console.log(keys);
-console.log(values);
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
